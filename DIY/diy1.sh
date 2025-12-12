35dc84feb126d73e1f52223545e81df9f45cfb1d
webtag-well <50027762+webtag-well@users.noreply.github.com>
2025-12-12 13:54:02 +0800

    Fix echo command for adding plugin repository

--- DIY/diy1.sh
+++ DIY/diy1.sh
@@ -6,4 +6,3 @@
 
 ##添加自己的插件库
 echo -e "\nsrc-git mzwrt_package https://github.com/mzwrt/mzwrt_package_Lite.git" >> feeds.conf.default
-echo -e "\nsrc-git mzwrt_package https://github.com/coolsnowwolf/lede.git" >> feeds.conf.default
